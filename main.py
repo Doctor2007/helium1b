f7e0cd819e3d5010acf976995fda49102c817357
Doctor2007 <andriy.kolisnyk2007@gmail.com>
2025-02-23 22:30:09 +0000

    Adjusted more accurately for the HS trade regulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 country_categories = {
     "European_Union": [
@@ -9,10 +9,8 @@ country_categories = {
         "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"
     ],
     
-    "UK": ["United Kingdom"],
-    
     "West_Not_EU": [
-        "United States", "Canada", "Australia", "New Zealand", "Norway", "Switzerland", "Iceland"
+        "United States of America", "United Kingdom", "Canada", "Australia", "New Zealand", "Norway", "Switzerland", "Iceland"
     ],
     
     "African": [
@@ -44,34 +42,57 @@ country_categories = {
 data = pd.read_csv('data/micro_data_trade_2022.csv')
 
 
-data_textile = data[data['HS2'] == 63]
+data_textile = data[data['HS4'] == 6309]
 
 data_textile_import = data_textile[data_textile['Tradeflow'] == 'Import']
 data_textile_export = data_textile[data_textile['Tradeflow'] == 'Import']
 
-data_textile.drop(columns=['Tradevalue_Cedis'], inplace=True)
-
-data_textile_import.to_csv('data/textile_trade_import_2022.csv', index=False)
-data_textile_export.to_csv('data/textile_trade_export_2022.csv', index=False)
+# data_textile_import.to_csv('data/textile_trade_import_2022.csv', index=False)
+# data_textile_export.to_csv('data/textile_trade_export_2022.csv', index=False)
 
 data_textile_import_eu = data_textile_import[data_textile_import['Partner_Country'].isin(country_categories['European_Union'])]
-data_textile_import_uk = data_textile_import[data_textile_import['Partner_Country'].isin(country_categories['UK'])]
 data_textile_import_west = data_textile_import[data_textile_import['Partner_Country'].isin(country_categories['West_Not_EU'])]
 data_textile_import_africa = data_textile_import[data_textile_import['Partner_Country'].isin(country_categories['African'])]
 data_textile_import_others = data_textile_import[data_textile_import['Partner_Country'].isin(country_categories['Others'])]
-data_textile_import_eu.to_csv('data/textile_trade_import_eu_2022.csv', index=False)
-data_textile_import_uk.to_csv('data/textile_trade_import_uk_2022.csv', index=False)
-data_textile_import_west.to_csv('data/textile_trade_import_west_2022.csv', index=False)
-data_textile_import_africa.to_csv('data/textile_trade_import_africa_2022.csv', index=False)
-data_textile_import_others.to_csv('data/textile_trade_import_others_2022.csv', index=False)
 
 
-import_list = [data_textile_import_eu, data_textile_import_uk, data_textile_import_west, data_textile_import_africa, data_textile_import_others]
-import_list_name = ['EU', 'UK', 'West', 'Africa', 'Others']
+import_list = [data_textile_import_eu, data_textile_import_west, data_textile_import_africa, data_textile_import_others]
+import_list_name = ['EU', 'West', 'Africa', 'Others']
 
-ind = 0
-for i in import_list:
-    sum_value = i['Tradevalue_USD'].sum()
-    print(import_list_name[ind], 'USD')
-    print(int(sum_value))
-    ind += 1
+def region_by_sum():
+    summary_data = []
+    ind = 0
+    for i in import_list:
+        sum_value = i['Tradevalue_USD'].sum()
+        summary_data.append({'Region': import_list_name[ind], 'Tradevalue_USD': int(sum_value)})
+        ind += 1
+
+    summary_df = pd.DataFrame(summary_data)
+
+    plt.bar(summary_df['Region'], summary_df['Tradevalue_USD'], color=['blue', 'orange', 'green', 'red', 'purple'])
+    plt.xlabel('Region')
+    plt.ylabel('Total Trade Value (USD)')
+    plt.title('Total Trade Value by Region')
+    plt.show()
+
+def region_leaders(region):
+    if region == 'World':
+        data = data_textile_import
+    else:
+        i = import_list_name.index(region)
+        data = import_list[i]
+    
+    grouped_df = data.groupby('Partner_Country', as_index=False)['Tradevalue_USD'].sum()
+    grouped_df['Tradevalue_USD'] = round(grouped_df['Tradevalue_USD'])
+    grouped_df = grouped_df.sort_values(by='Tradevalue_USD', ascending=False)
+    grouped_head = grouped_df.head(5)
+
+    plt.bar(grouped_head['Partner_Country'], grouped_head['Tradevalue_USD'], color=['blue', 'orange', 'green', 'red', 'purple'])
+    plt.xlabel('Region')
+    plt.ylabel('Total Trade Value (USD)')
+    plt.title('Total Trade Value by Region')
+    plt.show()
+
+# region_leaders('World')
+# region_leaders(region)
+region_by_sum()
